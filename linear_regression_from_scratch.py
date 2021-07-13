2183b38db3f804eee7e98a8269629a2159887915
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-07-13 20:52:51 +0100

    Adds validation set for testing Bias vs. Variance in the model.

--- linear_regression_from_scratch.py
+++ linear_regression_from_scratch.py
@@ -20,6 +20,11 @@ class LinearRegression:
         """
         return np.mean((y_hat - y)**2)
     
+    def _get_validation_loss(self, X_val, y_val):
+        y_hat_val = self.predict(X_val)
+        validation_loss = self._get_MSE(y_hat_val, y_val)
+        return np.mean(validation_loss)
+    
     def _get_gradients(self, X, y, y_hat):
         """
         Gets the gradients for the Linear Regression parameters
@@ -34,7 +39,8 @@ class LinearRegression:
         grad_b = 2 * np.mean(error)
         return grad_w, grad_b
 
-    def fit(self, X, y, lr = 0.001, epochs=100, print_loss=False):
+    def fit(self, X, y, X_val, y_val, lr = 0.001, epochs=1000,
+            acceptable_error=0.001, print_loss=False):
         """
         Optimises the Linear Regression parameters for the given data.
 
@@ -46,9 +52,11 @@ class LinearRegression:
                         default = 100
         """
         mean_loss = []
+        mean_validation_loss = []
         for epoch in range(epochs):
             minibatches = MiniBatch(X, y)
             loss_per_epoch = []
+            validation_loss_per_epoch = []
             for X_batch, y_batch in minibatches:
                 y_hat = self.predict(X_batch)
                 loss = self._get_MSE(y_hat, y_batch)
@@ -56,11 +64,21 @@ class LinearRegression:
                 self.w -= lr * grad_w
                 self.b -= lr * grad_b
                 loss_per_epoch.append(loss)
+                validation_loss_per_epoch.append(self._get_validation_loss(X_val, y_val))
             mean_loss.append(np.mean(loss_per_epoch))
+            mean_validation_loss.append(np.mean(validation_loss_per_epoch))
+
+            if epoch > 2 and abs(mean_validation_loss[-2]- mean_validation_loss[-1]) < acceptable_error:
+                print(f"Validation loss for epoch {epoch} is {mean_validation_loss[-1]}")
+                break
 
         if print_loss:
-            plt.plot(mean_loss)
+            # Plots the batch loss vs Validation loss
+            plt.plot(mean_loss, label="Minibatch loss")
+            plt.plot(mean_validation_loss, label="Validation loss")
+            plt.legend()
             plt.show()
+
 
     def predict(self, X):
         """
--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from sklearn import datasets
 import numpy as np
 from linear_regression_from_scratch import LinearRegression
 
-X, y = datasets.load_boston(return_X_y=True)
-X_mean = np.mean(X, axis=0)
-X_sd = np.std(X, axis=0)
-X = (X - X_mean)/X_sd
+def ScaleSplit(X, Y, size):
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = size, shuffle=True)
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size = size, shuffle=True)
+    
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler().fit(X_train)
+    X_train_sc = sc.transform(X_train)
+    X_test_sc = sc.transform(X_test)
+    X_val_sc = sc.transform(X_val)
 
-linear_model = LinearRegression(n_features=X.shape[1])
-linear_model.fit(X, y, print_loss=True)
-linear_model.predict(X)
+    return X_train_sc, X_test_sc, X_val_sc, Y_train, Y_test, Y_val
+
+X, y = datasets.load_boston(return_X_y=True)
+
+X_train, X_test, X_val, y_train, y_test, y_val = ScaleSplit(X, y, size=0.2)
+
+linear_model = LinearRegression(n_features=X_train.shape[1])
+linear_model.fit(X_train, y_train, X_val, y_val, print_loss=True)
+linear_model.predict(X_train)
