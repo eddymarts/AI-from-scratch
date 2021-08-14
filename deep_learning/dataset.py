de678bc17928f848e92da20e162478137920ce24
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-14 23:55:29 +0100

    Adds normalize and split methods to DataSet class.

--- deep_learning/dataset.py
+++ deep_learning/dataset.py
@@ -1,9 +1,12 @@
 import torch
+from torch.utils.data import *
 
-class Dataset(torch.utils.data.Dataset):
-  def __init__(self, X, y, normalize=False):
+class DataSet(Dataset):
+  """
+  Class that implements torch.utils.data.Dataset
+  """
+  def __init__(self, X, y, normalize=False, split=False, seed=None):
     super().__init__()
-    self.normalize = normalize
 
     if len(X.shape) > 1:
       self.n_features = X.shape[1]
@@ -22,13 +25,32 @@ class Dataset(torch.utils.data.Dataset):
     self.mean = torch.mean(self.X, axis=0)
     self.std = torch.std(self.X, axis=0)
 
-  def __getitem__(self, idx):
-    if self.normalize:
-      X = (self.X[idx] - self.mean)/self.std
-    else:
-      X = self.X[idx]
+    if normalize:
+      self.normalize()
 
-    return X, self.y[idx]
+    if split:
+      self.split(seed)
+
+  def __getitem__(self, idx):
+    return self.X[idx], self.y[idx]
 
   def __len__(self):
       return len(self.X)
+
+  def normalize(self, data=None):
+    if data == None:
+      self.X_raw = self.X
+      self.X = (self.X - self.mean)/self.std
+      return self.X
+    else:
+      return (data - self.mean)/self.std
+
+  def split(self, seed, sizes=[0.7, 0.15, 0.15], shuffle=True):
+    lengths = [round(len(self)*size) for size in sizes]
+    lengths[-1] = len(self) - sum(lengths[:-1])
+
+    if seed == None:
+      self.splits = random_split(self, lengths)
+    else:
+      self.splits = random_split(self, lengths,
+        generator=torch.Generator().manual_seed(seed))
