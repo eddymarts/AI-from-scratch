b99181e61dd893c45a911785f0c14b4fc80ae5b0
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-16 00:39:09 +0100

    Renames MNIST_demo.py to demo_CNNClassifier.py

--- deep_learning/demo_CNNClassifier.py
+++ deep_learning/demo_CNNClassifier.py
@@ -21,26 +21,15 @@ train_load = DataLoader(mnist_train, batch_size=16,
 test_load = DataLoader(mnist_test, batch_size=16,
             shuffle=False, num_workers=round(cpu_count()/2))
 
-X_val = mnist_test.test_data
-y_val = mnist_test.test_labels
-
-nn_classifier = CNNClassifier(X_val.shape[1], int(torch.max(y_val)+1))
+nn_classifier = CNNClassifier()
 loss = nn_classifier.fit(train_load, test_load, return_loss=True,
                             epochs=10, acceptable_error=0.0001, lr=0.01)
-for idx, (X_val, y_v) in enumerate(test_load):
-    y_hat = nn_classifier(X_val)
-    if idx == 0:
-        y_hat_val = nn_classifier(X_val)
-        y_val = y_v
-    else:
-        y_hat_val = torch.cat((y_hat_val, nn_classifier(X_val)), dim=0)
-        y_val = torch.cat((y_val, y_v), dim=0)
+y_val, y_hat_val = nn_classifier.predict(test_load, return_y=True)
 
-print(torch.cat((y_val.reshape(-1, 1), y_hat_val), dim=1)[0:10])
+print(torch.cat((y_val, y_hat_val), dim=1)[0:10])
 print("R^2 score:", r2_score(y_hat_val.detach().numpy(), y_val.detach().numpy()))
 plt.plot(loss['training'], label="Training set loss")
 plt.plot(loss['validation'], label="Validation set loss")
 plt.xlabel(f"Epochs\nl={loss['validation'][-1]}")
 plt.ylabel("CE")
 plt.show()
-# plt.savefig(fname="linear_regression.jpg")
