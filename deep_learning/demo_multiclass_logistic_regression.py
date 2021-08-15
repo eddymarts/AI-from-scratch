71921807c978d8a4e0cafc8daf427de8d5fe59e0
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-16 00:36:33 +0100

    Uses DataLoader for validation data.

--- deep_learning/demo_multiclass_logistic_regression.py
+++ deep_learning/demo_multiclass_logistic_regression.py
@@ -14,15 +14,16 @@ X, y = datasets.load_iris(return_X_y=True)
 iris_data = DataSet(X, y, normalize=True, split=True)
 train_load = DataLoader(iris_data.splits[0], batch_size=16,
             shuffle=True, num_workers=round(cpu_count()/2))
-
-X_val = iris_data.splits[1].dataset.X
-y_val = iris_data.splits[1].dataset.y
+test_load = DataLoader(iris_data.splits[1], batch_size=16,
+            shuffle=False, num_workers=round(cpu_count()/2))
 
 logistic_regressor = LogisticRegression(iris_data.n_features, int(torch.max(iris_data.y)+1))
-loss = logistic_regressor.fit(train_load, X_val, y_val, return_loss=True,
+loss = logistic_regressor.fit(train_load, test_load, return_loss=True,
                             epochs=1000, acceptable_error=0.0001, lr=0.001)
-y_hat_val = logistic_regressor(X_val)
-print(torch.cat((y_val, y_hat_val), dim=1))
+
+y_val, y_hat_val = logistic_regressor.predict(test_load, return_y=True)
+
+print(torch.cat((y_val, y_hat_val), dim=1)[0:10])
 print("R^2 score:", r2_score(y_hat_val.detach().numpy(), y_val.detach().numpy()))
 plt.plot(loss['training'], label="Training set loss")
 plt.plot(loss['validation'], label="Validation set loss")
