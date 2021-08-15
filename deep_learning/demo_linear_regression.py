4408c7cb19cd54dfaf5ae34b42b0b126190158ed
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-16 00:36:17 +0100

    Uses DataLoader for validation data.

--- deep_learning/demo_linear_regression.py
+++ deep_learning/demo_linear_regression.py
@@ -13,15 +13,15 @@ X, y = datasets.load_boston(return_X_y=True)
 boston_data = DataSet(X, y, normalize=True, split=True)
 train_load = DataLoader(boston_data.splits[0], batch_size=16,
             shuffle=True, num_workers=round(cpu_count()/2))
-
-X_val = boston_data.splits[1].dataset.X
-y_val = boston_data.splits[1].dataset.y
+test_load = DataLoader(boston_data.splits[1], batch_size=16,
+            shuffle=False, num_workers=round(cpu_count()/2))
 
 linear_regressor = LinearRegression(boston_data.n_features, boston_data.n_labels)
-loss = linear_regressor.fit(train_load, X_val, y_val, return_loss=True)
+loss = linear_regressor.fit(train_load, test_load, return_loss=True)
 
-y_hat_val = linear_regressor(X_val)
-print(torch.cat((y_val, y_hat_val), dim=1))
+y_val, y_hat_val = linear_regressor.predict(test_load, return_y=True)
+
+print(torch.cat((y_val, y_hat_val), dim=1)[0:10])
 print("R^2 score:", r2_score(y_hat_val.detach().numpy(), y_val.detach().numpy()))
 plt.plot(loss['training'], label="Training set loss")
 plt.plot(loss['validation'], label="Validation set loss")
