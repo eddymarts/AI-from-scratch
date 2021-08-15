6325823180f3ebf8ccde5558261e730c8f6264aa
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-15 01:23:17 +0100

    Implements Logistic Regression class.

--- deep_learning/linear_regression.py
+++ deep_learning/linear_regression.py
@@ -9,6 +9,9 @@ class LinearRegression(torch.nn.Module):
         
     def forward(self, X):
         return self.layers(X)
+    
+    def get_loss(self, y_hat, y):
+        return F.mse_loss(y_hat, y)
 
     def fit(self, data_load,  X_val, y_val, lr = 0.001, epochs=1000,
             acceptable_error=0.001, return_loss=False, save_every_epoch=None):
@@ -31,7 +34,7 @@ class LinearRegression(torch.nn.Module):
             for X_train, y_train in data_load:
                 optimiser.zero_grad()
                 y_hat = self(X_train)
-                loss = F.mse_loss(y_hat, y_train)
+                loss = self.get_loss(y_hat, y_train)
                 training_loss.append(loss.item())
                 loss.backward()
                 optimiser.step()
@@ -40,7 +43,7 @@ class LinearRegression(torch.nn.Module):
 
             if len(X_val) and len(y_val):
                 y_hat_val = self(X_val)
-                validation_loss.append(F.mse_loss(y_hat_val, y_val).detach().numpy())
+                validation_loss.append(self.get_loss(y_hat_val, y_val).detach().numpy())
 
                 if epoch > 2 and (
                     (abs(validation_loss[-2]- validation_loss[-1])/validation_loss[-1] < acceptable_error)
@@ -51,3 +54,13 @@ class LinearRegression(torch.nn.Module):
         if return_loss:
             return {'training': mean_training_loss,
                     'validation': validation_loss}
+
+class LogisticRegression(LinearRegression):
+    def __init__(self, n_features, n_labels):
+        super().__init__(n_features, n_labels)
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(n_features, n_labels), 
+            torch.nn.Sigmoid())
+    
+    def get_loss(self, y_hat, y):
+        return F.binary_cross_entropy(y_hat, y)
