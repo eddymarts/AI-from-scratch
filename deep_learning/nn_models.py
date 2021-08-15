a47fc952d257ae8c736280362242174f73357314
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-16 00:03:42 +0100

    Implements a Convolutional Neural Network for classification.

--- deep_learning/nn_models.py
+++ deep_learning/nn_models.py
@@ -1,5 +1,14 @@
 import torch
-from linear_models import LinearRegression
+import torch.nn.functional as F
+from linear_models import *
+
+class Reshape(torch.nn.Module):
+    def __init__(self, *args):
+        super(Reshape, self).__init__()
+        self.shape = args
+    
+    def forward(self, x):
+         return x.view(self.shape)
 
 class NeuralNetworkRegression(LinearRegression):
     def __init__(self, n_features, n_labels):
@@ -20,4 +29,45 @@ class NeuralNetworkRegression(LinearRegression):
             torch.nn.Linear(20, 10),
             torch.nn.ReLU(),
             torch.nn.Linear(10, n_labels)
+        )
+
+class NeuralNetworkClassification(LogisticRegression):
+    def __init__(self, n_features, n_labels):
+        super().__init__(n_features, n_labels)
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(n_features, 10),
+            torch.nn.ReLU(),
+            torch.nn.Linear(10, 20),
+            torch.nn.ReLU(),
+            torch.nn.Linear(20, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, 40),
+            torch.nn.ReLU(),
+            torch.nn.Linear(40, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, 20),
+            torch.nn.ReLU(),
+            torch.nn.Linear(20, 10),
+            torch.nn.ReLU(),
+            torch.nn.Linear(10, n_labels),
+            torch.nn.Softmax(1)
+        )
+
+class CNNClassifier(LogisticRegression):
+    def __init__(self, n_features, n_labels):
+        super().__init__(n_features, n_labels)
+        self.layers = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 10, kernel_size=5),
+            torch.nn.MaxPool2d(2),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(10, 20, kernel_size=5),
+            torch.nn.Dropout(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.ReLU(),
+            Reshape(-1, 320),
+            torch.nn.Linear(320, 50),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(),
+            torch.nn.Linear(50, 10),
+            torch.nn.LogSoftmax(1)
         )
