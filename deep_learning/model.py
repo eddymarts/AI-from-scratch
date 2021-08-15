3c219608cf691feaa93e2bb846b59e6af5180ecd
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-16 00:03:10 +0100

    Gets validation loss using a DataLoader object as validation data.

--- deep_learning/model.py
+++ deep_learning/model.py
@@ -24,46 +24,60 @@ class NeuralNetwork(torch.nn.Module):
         """
         return self.layers(X)
 
-    def fit(self, data_load,  X_val=None, y_val=None, lr = 0.001, epochs=1000,
+    def fit(self, train_load, test_load=None, X_val=None, y_val=None, lr = 0.001, epochs=1000,
             acceptable_error=0.001, return_loss=False, save_every_epoch=None):
         """
         Optimises the model parameters for the given data.
 
-        INPUTS: data_load -> torch.utils.data.DataLoader object with the data.
+        INPUTS: train_load -> torch.utils.data.DataLoader object with the data.
                 lr -> Learning Rate of Mini-batch Gradient Descent.
                         default = 0.001.
                 epochs -> Number of iterationns of Mini-Batch Gradient Descent.
                         default = 100
         """
-        self.train()
         optimiser = torch.optim.SGD(self.parameters(), lr=lr)
 
         mean_training_loss = []
-        validation_loss = []
+        mean_validation_loss = []
 
         for epoch in range(epochs):
             training_loss = []
-            for X_train, y_train in data_load:
+            self.train()
+            for X_train, y_train in train_load:
                 optimiser.zero_grad()
                 y_hat = self.forward(X_train)
-                loss = self.get_loss(y_hat, y_train)
-                training_loss.append(loss.item())
-                loss.backward()
+                train_loss = self.get_loss(y_hat, y_train)
+                training_loss.append(train_loss.item())
+                train_loss.backward()
                 optimiser.step()
             
             mean_training_loss.append(np.mean(training_loss))
 
-            if X_val and y_val:
-                y_hat_val = self.forward(X_val)
-                validation_loss.append(self.get_loss(y_hat_val, y_val).detach().numpy())
+            # if X_val and y_val:
+            #     y_hat_val = self.forward(X_val)
+            #     validation_loss.append(self.get_loss(y_hat_val, y_val).detach().numpy())
+
+            #     if epoch > 2 and (
+            #         (abs(validation_loss[-2]- validation_loss[-1])/validation_loss[-1] < acceptable_error)
+            #         or (validation_loss[-1] > validation_loss[-2])):
+            #         print(f"Validation train_loss for epoch {epoch} is {validation_loss[-1]}")
+            #         break
+            
+            if test_load:
+                validation_loss = []
+                self.eval() # set model in inference mode (need this because of dropout)
+                for X_val, y_val in test_load:
+                    y_hat_val = self.forward(X_val)
+                    val_loss = self.get_loss(y_hat_val, y_val)
+                    validation_loss.append(val_loss.item())
+                mean_validation_loss.append(np.mean(validation_loss))
 
                 if epoch > 2 and (
-                    (abs(validation_loss[-2]- validation_loss[-1])/validation_loss[-1] < acceptable_error)
-                    or (validation_loss[-1] > validation_loss[-2])):
-                    print(f"Validation loss for epoch {epoch} is {validation_loss[-1]}")
+                    (abs(mean_validation_loss[-2]- mean_validation_loss[-1])/mean_validation_loss[-1] < acceptable_error)
+                    or (mean_validation_loss[-1] > mean_validation_loss[-2])):
+                    print(f"Validation train_loss for epoch {epoch} is {mean_validation_loss[-1]}")
                     break
-        self.eval()
-        
+
         if return_loss:
             return {'training': mean_training_loss,
                     'validation': validation_loss}
