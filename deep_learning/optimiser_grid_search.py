453c012fce9b92cfa6d7d4a1daf1b1551ce3dfa5
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-16 20:58:06 +0100

    Grid search over optimisers for MNIST.

---
+++ deep_learning/optimiser_grid_search.py
@@ -0,0 +1,66 @@
+from nn_models import CNNClassifier
+from torchvision import datasets, transforms
+import torch
+from multiprocessing import cpu_count
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
+mnist_train = datasets.MNIST(root="datasets/mnist_train",
+                             download=True, train=True,
+                             transform=transforms.Compose([transforms.ToTensor(),
+                                       transforms.Normalize((0.1307,), (0.3081,))]))
+
+mnist_test = datasets.MNIST(root="datasets/mnist_test",
+                             download=True, train=False,
+                             transform=transforms.Compose([transforms.ToTensor(),
+                                       transforms.Normalize((0.1307,), (0.3081,))]))
+
+train_load = DataLoader(mnist_train, batch_size=16,
+            shuffle=True, num_workers=round(cpu_count()/2))
+test_load = DataLoader(mnist_test, batch_size=16,
+            shuffle=False, num_workers=round(cpu_count()/2))
+
+
+nn_sdg = CNNClassifier()
+sdg = torch.optim.SGD(nn_sdg.parameters(), lr=0.01)
+
+nn_sdg_m = CNNClassifier()
+sdg_m = torch.optim.SGD(nn_sdg.parameters(), lr=0.01, momentum=0.99)
+
+nn_sdg_nesterov = CNNClassifier()
+sdg_nesterov = torch.optim.SGD(nn_sdg_nesterov.parameters(), lr=0.01, momentum=0.99, nesterov=True)
+
+nn_adam = CNNClassifier()
+adam = torch.optim.AdamW(nn_adam.parameters(), lr=0.01, weight_decay=1e-4)
+
+models = {"SDG":nn_sdg,
+            "SDG_Momentum": nn_sdg_m,
+            "SDG_Nesterov": nn_sdg_nesterov,
+            "Adam": nn_adam}
+
+optimisers = {"SDG": sdg,
+                "SDG_Momentum": sdg_m,
+                "SDG_Nesterov": sdg_nesterov,
+                "Adam": adam}
+
+loss = {}
+y_val = {}
+y_hat_val={}
+for model in models.keys():
+    loss[model] = model.fit(train_load, test_load, return_loss=True,
+                            optimiser=optimisers[model],
+                            epochs=10, acceptable_error=0.0001, lr=0.01)
+
+    y_val[model], y_hat_val[model] = model.predict(test_load, return_y=True)
+
+
+
+print(torch.cat((y_val, y_hat_val), dim=1)[0:10])
+print("R^2 score:", r2_score(y_hat_val.detach().numpy(), y_val.detach().numpy()))
+plt.plot(loss['training'], label="Training set loss")
+plt.plot(loss['validation'], label="Validation set loss")
+plt.xlabel(f"Epochs\nl={loss['validation'][-1]}")
+plt.ylabel("CE")
+plt.legend()
+plt.show()
